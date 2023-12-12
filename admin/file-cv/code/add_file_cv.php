a9ca788d3cd9a4ced8140838e45a5264e96553bf
Hong Phong <hongphong2000p@gmail.com>
2023-12-12 18:47:39 +0700

    changes

--- admin/file-cv/code/add_file_cv.php
+++ admin/file-cv/code/add_file_cv.php
@@ -2,14 +2,9 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $valiFile = TRUE;
-    //$target_dir = "../../images/jobs-company/vietnam/";
-    $fileName = $_FILES['fileUpload']['name'];
-    $fileTmpName = $_FILES['fileUpload']['tmp_name'];
-    $cvContent = file_get_contents($fileTmpName);
-
     
-        echo 'check file cv: ' .$cvContent;
+
+        echo 'check file cv: ' .phpversion();
     
 }
 ?>
