3c68902938983cb177e08c6ca41d54b7dfd84bdc
Hong Phong <hongphong2000p@gmail.com>
2024-01-15 13:38:11 +0700

    fix css about, delete bs 3

--- hinhanh-video.php
+++ hinhanh-video.php
@@ -45,7 +45,6 @@ if (isset($_GET['type'])) {
     <link rel="icon" href="public/images/logo.png" type="image/x-icon" />
     <link rel="shortcut icon" type="image/x-icon" href="public/images/favicon.png" />
     <!-- Bao gồm các tệp CSS và JavaScript của Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
